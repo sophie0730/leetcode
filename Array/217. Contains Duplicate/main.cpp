bdd2e3dc3d516383f1068cd31ee6647a3153ba7f
Sophie <sophy010017@gmail.com>
2025-08-05 00:18:52 +0800

    Solve 217. Contains Duplicate, time complexity: O(n log n)

---
+++ Array/217. Contains Duplicate/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); //time complexity: O(n log n)
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
